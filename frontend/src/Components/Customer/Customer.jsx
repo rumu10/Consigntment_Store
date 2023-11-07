edfeb70a0fc36df82c13c131dbfd548a9769c0bc
Feiquanl <fliu6@wpi.edu>
2023-11-06 22:24:34 -0500

    roughly finished customer page:   frontend/src/Components/Customer/Customer.jsx

--- frontend/src/Components/Customer/Customer.jsx
+++ frontend/src/Components/Customer/Customer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Row, Col, Form, Input, InputNumber, Table, Select } from 'antd';
+import { Button, Row, Col, Form, Input, InputNumber, Table, Select, Checkbox, Divider, Flex, Space } from 'antd';
 import { useNavigate } from "react-router-dom";
 
 
@@ -12,28 +12,69 @@ const Customer = () => {
         // todo: add logic to add computer to the databas
     }
     const onFinish = (values) => {
-        console.log('Success:', values);
+        console.log('Success:', values.selectedStores);
+        // todo: add logic to generate inventory for these selected stores
     };
 
+
+    const [computerSelected] = Form.useForm();
+    //computerSelected.setFieldsValue({item: dataSource})
+
+    const tableRowSelection = {
+        onChange: (selectedRowKeys, selectedRows) => {
+            computerSelected.setFieldsValue({ selectedComputerKey: selectedRowKeys })
+            //            console.log(
+            //                `selectedRowKeys: ${selectedRowKeys}`,
+            //                "selectedRows: ",
+            //                selectedRows
+            //           )
+        },
+        getCheckboxProps: record => console.log(record)
+    };
+
+    const onFinishComputerSelection = (action) => {
+        const values = computerSelected.getFieldsValue();
+
+        console.log('Computer Selected:', values, action);
+
+        if (action == 'buy') {
+            console.log('Buy computer: ', values)
+        }
+        else {
+            console.log('Compare computer: ', values)
+        }
+    }
+
+    const onSignOut = () => {
+        navigate('/login')
+    }
+
+    // filter options
     const PriceOption = ['$2,001 or more', '$1,501 - $2,000', '$1,001 - $1,500', '$501 - $1000', '$500 or less']
     const MemoryOption = ["32 GB or more", "16 GB", "8 GB", "4 GB or less"]
     const StorageSizeOption = ["2 TB or more", "1 TB", "512 GB", "256 GB or less"]
     const ProcessorsOption = ["All Intel Processors", "All AMD Processors"]
     const ProcessGenerationsOption = ["13th Gen Intel", "12th Gen Intel", "11th Gen Intel", "AMD Ryzen 7000 Series", "AMD Ryzen 6000 Series"]
     const GraphicsOption = ["All NVIDIA Graphics", "All AMD Graphics", "All Intel Graphics"]
+
+
+    // computer list
+    // todo: currently these are mocked computer list data, we need to query these data from the database
     const dataSource = [
         {
             key: '1',
-            name: 'Computer 1',
+            computerName: 'Computer 1',
+            storeName: 'Store 3',
             conf: '32GB, 1TB, 12th Gen Intel i9, NVIDIA GeForce RTX 4090',
-            p: 20
+            p: 499.99
 
         },
         {
             key: '2',
-            name: 'Computer 2',
+            computerName: 'Computer 2',
+            storeName: 'Store 4',
             conf: '8GB, 1TB, 11th Gen Intel i9, NVIDIA GeForce RTX 4080',
-            p: 30
+            p: 329.99
 
         },
     ];
@@ -41,10 +82,17 @@ const Customer = () => {
     const columns = [
         {
             title: 'Computer',
-            dataIndex: 'name',
+            dataIndex: 'computerName',
             key: 'name',
             width: '25%'
         },
+        {
+            title: 'Store',
+            dataIndex: 'storeName',
+            key: 'name',
+            width: '25%'
+        },
+
         {
             title: 'Configuration',
             dataIndex: 'conf',
@@ -57,20 +105,50 @@ const Customer = () => {
             key: 'name',
             width: '25%'
         },
+
+
+    ];
+
+    const storeColumns = [
         {
-            title: '',
-            width: '25%',
-            dataIndex: '',
-            key: 'x',
-            render: () => <Button type="primary">Buy Computer</Button>,
+            title: "Name",
+            dataIndex: "name",
+            render: (text) => <a href="#">{text}</a>
         },
 
     ];
 
+    // store list
+    // todo: currently these are mocked store list data, we need to query these data from the database
+    const storeData = [
+        {
+            key: "1",
+            name: "Store 1",
+        },
+        {
+            key: "2",
+            name: "Store 2",
+        },
+        {
+            key: "3",
+            name: "Store 3",
+        },
+        {
+            key: "4",
+            name: "Store 4",
+        },
+        {
+            key: "5",
+            name: "Store 5",
+        },
+        {
+            key: "6",
+            name: "Store 6",
+        },
+    ]
 
-    const onSignOut = () => {
-        navigate('/login')
-    }
+
+
 
     return (
         <div className='customer' style={{ margin: '30px' }}>
@@ -88,7 +166,7 @@ const Customer = () => {
                         autoComplete="off"
                     >
 
-                        <h2 style={{ textAlign: 'center' }}>Find Computers</h2>
+                        <h2 style={{ textAlign: 'center' }}>Computer Filters</h2>
 
                         <Form.Item
                             name="price"
@@ -186,15 +264,8 @@ const Customer = () => {
                             </Select>
                         </Form.Item>
 
-
-
-
-
-
-
-
                         <Form.Item >
-                            <Button type="primary" htmlType="submit">
+                            <Button type="primary" htmlType="submit" >
                                 Find Computers
                             </Button>
                         </Form.Item>
@@ -204,7 +275,40 @@ const Customer = () => {
                 </Col>
                 <Col className="gutter-row" lg={{ span: 8, offset: 1 }}>
                     <h2 style={{ textAlign: 'center' }}>Computer List</h2>
-                    <Table dataSource={dataSource} columns={columns} />
+
+
+
+
+                    <Form form={computerSelected}>
+                        <Form.Item name="selectedComputerKey">
+                            <Table rowSelection={tableRowSelection} rowKey={(dataSource) => dataSource.key} dataSource={dataSource} columns={columns} />
+                        </Form.Item>
+
+                        <Flex gap="small" wrap="wrap" style={{ marginLeft: '150px' }}>
+                            <Form.Item
+                                wrapperCol={{
+                                    offset: 8,
+                                    span: 16,
+                                }}
+                            >
+                                <Button type="primary" name="SiteManager" htmlType="submit" onClick={e => onFinishComputerSelection('buy')}>
+                                    Buy Selected
+                                </Button>
+                            </Form.Item>
+                            <Form.Item
+                                wrapperCol={{
+                                    offset: 8,
+                                    span: 16,
+                                }}
+                            >
+                                <Button type="primary" name="Store" htmlType="submit" onClick={e => onFinishComputerSelection('compare')}>
+                                    Compare Selected
+                                </Button>
+                            </Form.Item>
+                        </Flex>
+                    </Form>
+                    <br></br>
+
                 </Col>
 
 
@@ -212,6 +316,35 @@ const Customer = () => {
                     <Button type="primary" block onClick={onSignOut}>
                         SignOut
                     </Button>
+                    <Divider />
+
+
+
+
+                    <Form
+                        name="storeList"
+                        onFinish={onFinish}
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                        layout={'vertical'}
+                        style={{ maxWidth: 600 }}
+                    >
+
+                        <h2 style={{ textAlign: 'center' }}>Store List by distance</h2>
+                        <Form.Item name="selectedStores" wrapperCol={{ span: 8 }}>
+                            <Checkbox.Group options={storeData.map((p) => ({ label: p.name, value: p.key }))}>
+                            </Checkbox.Group>
+                        </Form.Item>
+                        <Form.Item label=" " colon={false}>
+                            <Button type="primary" htmlType="submit">
+                                Generate Inventory
+                            </Button>
+                        </Form.Item>
+
+
+
+                    </Form>
+
 
                 </Col>
             </Row>
