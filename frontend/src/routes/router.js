d118252294ab6ac1fc688f3ca4032ef403deca77
jaimebw <jaimebwv@gmail.com>
2023-11-06 22:58:24 -0500

    Corrected serverless app to work. Added manual routing, may change it.

--- frontend/src/routes/router.js
+++ frontend/src/routes/router.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import {CreateStore} from '../Components/CreateStore/CreateStore'
+import {Customer} from '../Components/Customer/Customer'
+import {Login} from '../Components/Login/Login'
+import {SiteManager} from '../Components/SiteManager/SiteManager'
+import {StoreOwner} from '../Components/StoreOwner/StoreOwner'
+
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 export default[
 
-	<Route exact path="/manager/home/audiopage/:id" component={AudioPage} key={2} />
+
+<Router>
+    <Route path="/createstore" component={CreateStore} />
+    <Route path="/customer" component={Customer} />
+    <Route path="/login" component={Login} />
+    <Route path="/sitemanager" component={SiteManager} />
+    <Route path="/storeowner" component={StoreOwner} />
+</Router>
+
+
 
 ]
